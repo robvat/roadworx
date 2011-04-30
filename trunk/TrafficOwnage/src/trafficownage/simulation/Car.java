4d859e5d9263fe85bdc4f604657804e333b28f5b
gerritdrost <gerritdrost@876ce411-de22-ebdd-af3c-dc7789ea712b>
2011-04-30 11:25:39 +0000

    - Rewrote a lot of stuff. Did not test it yet, but the driver model should be implemented now. Stuff to do: programm a leading car. This is why I couldn't test yet.
    The code allows us to define different cars and driver types. The implemented driver model is the IDM, from the paper on dropbox. Other driver models can be implemented easily as well, if required.

--- trunk/TrafficOwnage/src/trafficownage/simulation/Car.java
+++ trunk/TrafficOwnage/src/trafficownage/simulation/Car.java
@@ -9,53 +9,105 @@ package trafficownage.simulation;
  *
  * @author Gerrit
  */
-public class Car implements Updatable {
+public class Car {
     private CarType carType;
     private DriverType driverType;
 
-    private float velocity;
-    private float acceleration;
+    private DriverModel driverModel;
 
-    private float position;
+    private double max_velocity;
 
+    private double velocity;
+    private double acceleration;
+
+    private double position;
+
+    private class IDM implements DriverModel {
+        private double a,b,v0,s0,T;
+
+        public void init(DriverType driver, CarType car, double initial_max_velocity) {
+            a = Math.min(driver.getMaxAcceleration(), car.getMaxAcceleration());
+            b = driver.getMaxComfortableDeceleration();
+            v0 = initial_max_velocity;
+            s0 = driver.getMinimumDistanceToLeader();
+            T = driver.getDesiredTimeHeadway();
+        }
+
+        private double desired_distance;
+        public double update(double velocity_leader, double distance_to_leader) {
+            desired_distance = s0 + (velocity * T) + ((velocity * velocity_leader) / (2 * Math.sqrt(a*b)));
+            return a * (1 - Math.pow((velocity/v0),4.0) - (desired_distance / distance_to_leader));
+
+        }
+
+        public void setMaxVelocity(double max_velocity) {
+            v0 = max_velocity;
+        }
+
+    }
+
+    /**
+     * Initializes the car. For this we need to know the driver and the car
+     * @param carType
+     * @param driverType
+     */
     public Car(CarType carType, DriverType driverType) {
         this.carType = carType;
         this.driverType = driverType;
+
+        driverModel = new IDM();
     }
-    
+
+
+    /**
+     * Initializes the car. The car needs to know some stuff like the maximum
+     * allowed velocity to start driving properly.
+     * @param max_velocity
+     */
+    public void init(double max_velocity) {
+        this.max_velocity = max_velocity;
+    }
+
+
+    /**
+     * Sets the maximum allowed velocity.
+     * @param max_velocity
+     */
+    public void setMaxVelocity(double max_velocity) {
+        this.max_velocity = max_velocity;
+    }
+
     /**
      * @return the velocity
      */
-    public float getVelocity() {
+    public double getVelocity() {
         return velocity;
     }
 
     /**
      * @return the acceleration
      */
-    public float getAcceleration() {
+    public double getAcceleration() {
         return acceleration;
     }
 
     /**
      * @return the position
      */
-    public float getPosition() {
+    public double getPosition() {
         return position;
     }
 
-    public void update(int dT) {
-        //TODO: Update the cars position
-        float dVtoLeader = 50f - velocity;
-        float dPtoLeader = 20f;
+    /**
+     * Updates the car position and velocity
+     * @param dT
+     * @param velocity_leader
+     * @param distance_to_leader
+     */
+    public void update(double timestep, double velocity_leader, double distance_to_leader) {
+        position += velocity * timestep;
+        velocity += acceleration * timestep;
+        acceleration = driverModel.update(velocity_leader, distance_to_leader);
 
-        float s = driverType.getMinimumDistanceToLeader() +
-                (velocity*driverType.getDesiredTimeHeadway()) +
-                ((velocity * dVtoLeader) / (2 * (float)Math.sqrt(driverType.getMaxAcceleration() * driverType.getMaxComfortableDeceleration())));
-
-        float acc = driverType.getMaxAcceleration() * (float)Math.abs(1 - Math.pow((velocity / 50f),4) - (s/dPtoLeader));
-
-        acceleration = acc;
-        velocity = (acceleration / 1000f) * (float)dT;
     }
 }
