5e16db0af3ca3f47cf8c3dc4e170c8bbee320594
gerritdrost@gmail.com <gerritdrost@gmail.com@876ce411-de22-ebdd-af3c-dc7789ea712b>
2011-05-02 12:10:09 +0000

    Not only one, but several cars driving on a lane. The lane is represented by a row of equal signs, and cars are represented by stars. Because of the limited resolutions, cars in queue might be represented by just one star.

--- trunk/TrafficOwnage/src/trafficownage/simulation/Main.java
+++ trunk/TrafficOwnage/src/trafficownage/simulation/Main.java
@@ -11,7 +11,9 @@ package trafficownage.simulation;
  */
 public class Main {
     public static void main(String[] args) {
-        Lane l = new Lane(100000.0, 50.0);
+        double LANE_LENGTH = 10000.0;
+
+        Lane l = new Lane(LANE_LENGTH, 50.0);
 
         Car c = new Car();
         c.init(CarType.CAR, DriverType.NORMAL);
@@ -19,9 +21,20 @@ public class Main {
 
         double time_passed = 0.0;
         double TIMESTEP = 0.1;
-        for (int i = 0; i < 10000; i++) {
+
+        //let them drive for 5 minutes
+        while (time_passed < (60 * 5)) {
             l.update(TIMESTEP);
-            System.out.println(time_passed + ": " + l.toString());
+            System.out.println(l.toString());
+
+            //when the last car is at position 100m, a new car is added
+            if (c.getPosition() > 100.0) {
+                c = new Car();
+                c.init(CarType.CAR, DriverType.NORMAL);
+                l.addCar(c);
+            }
+
+
             time_passed += TIMESTEP;
         }
 
