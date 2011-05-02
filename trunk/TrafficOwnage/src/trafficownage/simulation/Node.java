2af6ba594c9a4cfe6309ccb2688b0971631f86a7
f.vandenheuvel@student.maastrichtuniversity.nl <f.vandenheuvel@student.maastrichtuniversity.nl@876ce411-de22-ebdd-af3c-dc7789ea712b>
2011-05-02 15:39:45 +0000

    More work done on Node & Roundabout, more comments aswell :)

--- trunk/TrafficOwnage/src/trafficownage/simulation/Node.java
+++ trunk/TrafficOwnage/src/trafficownage/simulation/Node.java
@@ -8,18 +8,22 @@ package trafficownage.simulation;
  *
  * @author Gerrit
  */
-public interface Node {
+public abstract class Node
+{
+    
 
-    boolean drivethrough(Car incoming);
+    /* incoming cars need to know wether to brake or continue driving */
+    abstract boolean drivethrough(Car incoming);
     /* TODO: crossroads without lights need drivers to check so drivers
      need a function for this that will have to be called by that node */
 
+    
     /* once a car is at the border it has to be accepted
      by the new node and leave the old road */
-    void AcceptCar(Car incoming);
+    abstract void acceptCar(Car incoming);
 
     /* Cars can be on a node for a longer time so nodes need to be
      updated aswell (if a car has to be 40 sec on a node then the node needs to
      know the time */
-    void Update();
+    abstract void update();
 }
