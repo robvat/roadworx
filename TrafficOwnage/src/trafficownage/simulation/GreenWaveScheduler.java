41c7ce1a2a1d6695a9a3e13c57c3ed9591b5d491
f.vandenheuvel@student.maastrichtuniversity.nl <f.vandenheuvel@student.maastrichtuniversity.nl@876ce411-de22-ebdd-af3c-dc7789ea712b>
2011-08-21 22:19:07 +0000

    TrafficLight can now take GreenWaveScheduler into account And The Scheduler inself is changed (a bit). Still needs to figure out which lanes (and for how long they) need to be green

--- TrafficOwnage/src/trafficownage/simulation/GreenWaveScheduler.java
+++ TrafficOwnage/src/trafficownage/simulation/GreenWaveScheduler.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package trafficownage.simulation;
 
 import java.util.ArrayList;
@@ -12,24 +11,28 @@ import java.util.List;
  *
  * @author BeerBrewer
  */
-public class GreenWaveScheduler {
+public class GreenWaveScheduler
+{
 
     private List<GreenWave> greenWaves; //each entry is a list of subsequent roads
     private List<List<Node>> nodeLists; //each entry is a road, made up from nodes
 
-    public GreenWaveScheduler(){
-
+    public GreenWaveScheduler()
+    {
     }
 
     // in here we need something to determine subsequent roads
-    public void init(List<Road> roads){
+    public void init(List<Road> roads)
+    {
         nodeLists = new ArrayList<List<Node>>();
-        for (Road r : roads) {
-                nodeLists.add(r.getNodes());
-            }
-        
+        for (Road r : roads)
+        {
+            nodeLists.add(r.getNodes());
+        }
+
         greenWaves = new ArrayList<GreenWave>();
-        for (List<Node> ln : nodeLists){
+        for (List<Node> ln : nodeLists)
+        {
             GreenWave gv = new GreenWave();
             gv.init(ln);
             greenWaves.add(gv);
@@ -40,62 +43,85 @@ public class GreenWaveScheduler {
     //this class consists of a list of subsequent roads and operators on it.
     private class GreenWave
     {
+
         private int redNode = 0;
-        private int nextNode =0;
+        private int nextNode = 0;
         private double counter;
         private static final double OVERLAP_TIME = 3.0;
-        private List<Node> ln;
+        private List<TrafficLight> trafficLightList;
         private List<Double> trafficLightGreen;//list of when a certain traffic light becomes green
         private List<Double> trafficLightRed; //list of when a certain traffic light becomes red
 
-        public void init(List<Node> ln){
-            this.ln = ln;
+        public void init(List<Node> ln)
+        {
+            for(Node p : ln)
+            {
+                if(p instanceof TrafficLight)
+                    trafficLightList.add((TrafficLight) p);
+                else
+                    System.err.print("A non-trafficlight node was found in greenwave");
+            }
             trafficLightGreen = new ArrayList<Double>();
             trafficLightRed = new ArrayList<Double>();
-            counter = 0;            
+            counter = 0;
             double trafficTime = 0.0; //time traffic needs between two node
-            for (Node n : ln){
-                if (ln.get(0) == n){
+            for (Node n : ln)
+            {
+                if (ln.get(0) == n)
+                {
                     counter = 0;
-                    trafficTime = n.distanceTo(ln.get(1))/n.getRoadSegment(ln.get(1)).getMaxVelocity();
+                    trafficTime = n.distanceTo(ln.get(1)) / n.getRoadSegment(ln.get(1)).getMaxVelocity();
                     trafficLightGreen.add(0.0);
+                } else
+                {
+                    trafficLightGreen.add((ln.indexOf(n) * trafficTime) - OVERLAP_TIME);
                 }
-                else
-                    trafficLightGreen.add((ln.indexOf(n)*trafficTime) - OVERLAP_TIME);                
-                }
-            
+            }
+
         }
 
         //this method initializes the green wave, which means it starts the counter of a green wave
-        public void startWave(){
+        public void startWave()
+        {
             counter = 0;
             nextNode = 0;
             redNode = 0;
-            TrafficLight x = (TrafficLight) ln.get(nextNode);
+            TrafficLight x = (TrafficLight) trafficLightList.get(nextNode);
             double redTime = x.getGreenTime();
-            for (Node n: ln){
-                trafficLightRed.add(trafficLightGreen.get(ln.indexOf(n)) + redTime + OVERLAP_TIME);
+            for (Node n : trafficLightList)
+            {
+                trafficLightRed.add(trafficLightGreen.get(trafficLightList.indexOf(n)) + redTime + OVERLAP_TIME);
             }
         }
 
-
-        public void update(double timeStep) {
+        public void update(double timeStep)
+        {
             System.out.println(timeStep);
             counter += timeStep;
-            if (counter >= trafficLightGreen.get(nextNode)){
-                TrafficLight x = (TrafficLight) ln.get(nextNode);
-                x.setGreen();
+            if (counter >= trafficLightGreen.get(nextNode))
+            {
+                TrafficLight x = trafficLightList.get(nextNode);
+                List<Lane> greenLanes = null;
+                double greenTime = 0.0;
+                // TODO: Time to find out what these 2 need to be!
+                x.setGreen(greenLanes, greenTime);
                 nextNode++;
             }
-            if (counter >= trafficLightGreen.get(redNode)){{
-                TrafficLight x = (TrafficLight) ln.get(redNode);
-                x.setRed();
-                redNode++;
+
+            /*
+             * Dont think we need redNodes, they get turned back to normal
+             * after the greentime is done
+             *
+            if (counter >= trafficLightGreen.get(redNode))
+            {
+                {
+                    TrafficLight x = trafficLightList.get(redNode);
+                    x.setRed();
+                    redNode++;
+                }
             }
+            */
+
         }
-
-
     }
-
-}
 }
